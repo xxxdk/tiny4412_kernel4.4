6d540dd8f46077d679ed9ae07091356f3e5aac25
xxxdk <953954661@qq.com>
2018-05-28 23:14:48 -0700

    change uboot src

---
+++ uboot_tiny4412/include/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "May 27 2018"
+#define U_BOOT_TIME "20:43:11"
---
+++ uboot_tiny4412/lib/asm-offsets.s
@@ -0,0 +1,240 @@
+	.arch armv7-a
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 4
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"asm-offsets.c"
+	.text
+.Ltext0:
+	.cfi_sections	.debug_frame
+	.section	.text.startup,"ax",%progbits
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+.LFB71:
+	.file 1 "lib/asm-offsets.c"
+	.loc 1 23 0
+	.cfi_startproc
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	.loc 1 25 0
+#APP
+@ 25 "lib/asm-offsets.c" 1
+	
+->GENERATED_GBL_DATA_SIZE #128 (sizeof(struct global_data)+15) & ~15
+@ 0 "" 2
+	.loc 1 29 0
+	mov	r0, #0
+	bx	lr
+	.cfi_endproc
+.LFE71:
+	.size	main, .-main
+	.text
+.Letext0:
+	.section	.debug_info,"",%progbits
+.Ldebug_info0:
+	.4byte	0x8b
+	.2byte	0x4
+	.4byte	.Ldebug_abbrev0
+	.byte	0x4
+	.uleb128 0x1
+	.4byte	.LASF11
+	.byte	0x1
+	.4byte	.LASF12
+	.4byte	.LASF13
+	.4byte	.Ldebug_ranges0+0
+	.4byte	0
+	.4byte	.Ldebug_line0
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x8
+	.4byte	.LASF0
+	.uleb128 0x2
+	.byte	0x4
+	.byte	0x7
+	.4byte	.LASF1
+	.uleb128 0x2
+	.byte	0x2
+	.byte	0x7
+	.4byte	.LASF2
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x6
+	.4byte	.LASF3
+	.uleb128 0x2
+	.byte	0x2
+	.byte	0x5
+	.4byte	.LASF4
+	.uleb128 0x3
+	.byte	0x4
+	.byte	0x5
+	.ascii	"int\000"
+	.uleb128 0x2
+	.byte	0x4
+	.byte	0x7
+	.4byte	.LASF5
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x5
+	.4byte	.LASF6
+	.uleb128 0x2
+	.byte	0x8
+	.byte	0x7
+	.4byte	.LASF7
+	.uleb128 0x2
+	.byte	0x4
+	.byte	0x7
+	.4byte	.LASF8
+	.uleb128 0x2
+	.byte	0x4
+	.byte	0x5
+	.4byte	.LASF9
+	.uleb128 0x2
+	.byte	0x1
+	.byte	0x8
+	.4byte	.LASF10
+	.uleb128 0x4
+	.4byte	.LASF14
+	.byte	0x1
+	.byte	0x16
+	.4byte	0x48
+	.4byte	.LFB71
+	.4byte	.LFE71-.LFB71
+	.uleb128 0x1
+	.byte	0x9c
+	.byte	0
+	.section	.debug_abbrev,"",%progbits
+.Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x11
+	.byte	0x1
+	.uleb128 0x25
+	.uleb128 0xe
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x1b
+	.uleb128 0xe
+	.uleb128 0x55
+	.uleb128 0x17
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x10
+	.uleb128 0x17
+	.byte	0
+	.byte	0
+	.uleb128 0x2
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.byte	0
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0x8
+	.byte	0
+	.byte	0
+	.uleb128 0x4
+	.uleb128 0x2e
+	.byte	0
+	.uleb128 0x3f
+	.uleb128 0x19
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x27
+	.uleb128 0x19
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x6
+	.uleb128 0x40
+	.uleb128 0x18
+	.uleb128 0x2117
+	.uleb128 0x19
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",%progbits
+	.4byte	0x1c
+	.2byte	0x2
+	.4byte	.Ldebug_info0
+	.byte	0x4
+	.byte	0
+	.2byte	0
+	.2byte	0
+	.4byte	.LFB71
+	.4byte	.LFE71-.LFB71
+	.4byte	0
+	.4byte	0
+	.section	.debug_ranges,"",%progbits
+.Ldebug_ranges0:
+	.4byte	.LFB71
+	.4byte	.LFE71
+	.4byte	0
+	.4byte	0
+	.section	.debug_line,"",%progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",%progbits,1
+.LASF6:
+	.ascii	"long long int\000"
+.LASF5:
+	.ascii	"unsigned int\000"
+.LASF14:
+	.ascii	"main\000"
+.LASF1:
+	.ascii	"long unsigned int\000"
+.LASF11:
+	.ascii	"GNU C 4.8.3 20140320 (prerelease) -mfloat-abi=soft "
+	.ascii	"-marm -mabi=aapcs-linux -mno-thumb-interwork -march"
+	.ascii	"=armv7-a -mtls-dialect=gnu -g -Os -fno-common -ffix"
+	.ascii	"ed-r8 -fno-builtin -ffreestanding -fno-stack-protec"
+	.ascii	"tor\000"
+.LASF7:
+	.ascii	"long long unsigned int\000"
+.LASF0:
+	.ascii	"unsigned char\000"
+.LASF12:
+	.ascii	"lib/asm-offsets.c\000"
+.LASF10:
+	.ascii	"char\000"
+.LASF13:
+	.ascii	"/home/project/workSpace/tiny4412_kernel4.4/uboot_ti"
+	.ascii	"ny4412\000"
+.LASF9:
+	.ascii	"long int\000"
+.LASF2:
+	.ascii	"short unsigned int\000"
+.LASF3:
+	.ascii	"signed char\000"
+.LASF4:
+	.ascii	"short int\000"
+.LASF8:
+	.ascii	"sizetype\000"
+	.ident	"GCC: (Sourcery CodeBench Lite 2014.05-29) 4.8.3 20140320 (prerelease)"
+	.section	.note.GNU-stack,"",%progbits
