15f59a88cf7bd21ccfd3e8a9001a12cd06051050
xxxdk <953954661@qq.com>
2018-06-08 03:45:08 -0700

    add argv

--- dnw/dnw.c
+++ dnw/dnw.c
@@ -14,13 +14,14 @@
 #define TINY4412_SECBULK_IDVENDOR   0x04e8
 #define TINY4412_SECBULK_IDPRODUCT  0x1234
 
-#define TINY4412_RAM_BASE       0x60000000
+// #define TINY4412_RAM_BASE       0x60000000
 
 // TINY4412
-#define RAM_BASE            TINY4412_RAM_BASE
+// #define RAM_BASE            TINY4412_RAM_BASE
 #define VENDOR_ID           TINY4412_SECBULK_IDVENDOR
 #define PRODUCT_ID          TINY4412_SECBULK_IDPRODUCT
 
+static long ram_base = 0x40000000;
 
 struct usb_dev_handle * open_port()
 {
@@ -66,7 +67,7 @@ struct usb_dev_handle * open_port()
 
 void usage()
 {
-    printf("Usage: dnw2 <file>\n\n");
+    printf("Usage: dwn [-a download_addr] <-f filename>\n\n");
 }
 
 unsigned char* prepare_write_buf(char *filename, unsigned int *len)
@@ -107,7 +108,7 @@ unsigned char* prepare_write_buf(char *filename, unsigned int *len)
     printf("Filesize : %d bytes\n", (int)fs.st_size);
     printf ("Sum is %x\n",sum);
 
-    *((u_int32_t*)write_buf) = RAM_BASE;        //download address
+    *((u_int32_t*)write_buf) = ram_base;        //download address
     *((u_int32_t*)write_buf+1) = fs.st_size + 10;    //download size;
     write_buf [fs.st_size + 8] = sum & 0xff;
     write_buf [fs.st_size + 9] = sum >> 8;
@@ -124,7 +125,14 @@ error:
 
 int main(int argc, char *argv[])
 {
-    if(2!=argc)
+    int oc;
+    long addr = 0;
+    unsigned int len = 0;
+    unsigned int remain = 0;
+    unsigned int towrite;
+    unsigned char* write_buf;
+
+    if(5!=argc)
     {
         usage();
         return 1;
@@ -136,29 +144,61 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    unsigned int len = 0;
-    unsigned char* write_buf = prepare_write_buf(argv[1], &len);
-    if(NULL==write_buf) return 1;
+    while ((oc = getopt(argc, argv, "ha:f:")) != -1){
+      switch (oc){
+        case 'h':
+          usage();
+          return 0;
 
-    unsigned int remain = len;
-    unsigned int towrite;
-    printf("Writing data ...\n");
-    while(remain)
-    {
-        towrite = remain>512 ? 512 : remain;
-        printf("towrite %d\n", towrite);
-        if(towrite != usb_bulk_write(hdev, 0x02, write_buf+(len-remain), towrite, 3000))
-        {
-            printf("\ntowrite %d\n", towrite);
-            perror("usb_bulk_write failed");
-            break;
-        }
-        remain-=towrite;
-        printf("towrite %d\n", towrite);
-        printf("remain %d\n", remain);
-        printf("\r %d%%\t %d bytes    ", (len-remain)*100/len, len-remain);
-        fflush(stdout);
+        case 'a':
+          // addr may be hex or dec
+          if (optarg[0]=='0' && (optarg[1]=='x'|| optarg[1]=='X')){
+            sscanf(optarg, "%lx", &addr);
+            printf("get hex addr: 0x%lx\n", addr);
+          }
+          else{
+            addr = atoll(optarg);
+          }
+
+          // check addr
+          if (addr <=0 || addr>0xfffffffe){
+            printf("Please check your addr(0x%lx), it may be wrong!", addr);
+            return -1;
+          }
+          else
+          {
+            ram_base = addr;
+          }
+          break;
+
+        case 'f':
+          write_buf = prepare_write_buf(optarg, &len);
+          if(NULL==write_buf) return 1;
+          remain = len;
+          printf("Writing data ...\n");
+          while(remain)
+          {
+              towrite = remain>512 ? 512 : remain;
+              printf("towrite %d\n", towrite);
+              if(towrite != usb_bulk_write(hdev, 0x02, write_buf+(len-remain), towrite, 3000))
+              {
+                  printf("\ntowrite %d\n", towrite);
+                  perror("usb_bulk_write failed");
+                  break;
+              }
+              remain-=towrite;
+              printf("towrite %d\n", towrite);
+              printf("remain %d\n", remain);
+              printf("\r %d%%\t %d bytes    ", (len-remain)*100/len, len-remain);
+              fflush(stdout);
+          }
+          if(0==remain) printf("Done!\n");
+          return 0;
+
+        default:
+          printf("Unknow options, please see help \n");
+          usage();
+          return -1;
+      }
     }
-    if(0==remain) printf("Done!\n");
-    return 0;
 }
